12ab94df257359c01c8b7e4b09d30a55ded3beb4
codeplayerdds <929330564@qq.com>
2021-11-03 20:44:55 +0800

    feat: 添加单元测试

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-01 13:47:44
+ * @LastEditTime: 2021-11-03 09:44:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \code-testing\jest.config.js
+ */
 module.exports = {
   // u can change this option to a more specific folder for test single component or util when dev
   // for example, ['<rootDir>/packages/components/button']
   roots: ['<rootDir>/src/'],
   setupFiles: ['<rootDir>/jest.setup.js'],
 
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|style|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.(t|j)sx?$': [
       'babel-jest',
       {
